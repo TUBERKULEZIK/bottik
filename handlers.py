64f7a62694b651b6cacc3096461a3c2d41abeda2
TUBERKULEZ <dihebzi871@gmail.com>
2026-01-31 01:51:13 +0300

    Add files via upload

---
+++ handlers.py
@@ -0,0 +1,23 @@
+from aiogram.types import Message, ContentType 
+from aiogram import Bot, F, Router, types
+from aiogram.filters import CommandStart, Command 
+from aiogram.enums import ParseMode
+
+
+router = Router()
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer('Привет друг, я бот для пробива Искажение👋.')
+    await message.reply('Напиши /search для начала поиска.')
+@router.message(Command('search'))
+async def handle_message(message: types.Message):
+    await message.answer("Отлично вы вошли в режим поиска, пришлите мне номер телефона в таком формате:"  "<code>79991234567</code>", parse_mode=ParseMode.HTML)
+
+@router.message(F.text.regexp(r'^\d{10,12}$'))
+async def handle_phone(message: types.Message):
+    # Сначала подтверждаем действие
+    await message.answer("Поиск начался...")
+@router.message()
+async def wrong_format(message: types.Message):
+    await message.answer("Ошибка! Введите номер телефона (только цифры, без букв и символов).")
